be072da4d3e20f991e6f545010fdf9e06c8f45b9
ffrrpp <ffrrpp1@gmail.com>
2019-09-26 23:36:59 -0400

    week3

--- webapp/views.py
+++ webapp/views.py
@@ -10,35 +10,72 @@ from flask import render_template
 from webapp import app
 from webapp.a_model import ModelIt
 import pandas as pd
+import numpy as np
 from flask import request
+import pickle
+import datetime
+import sklearn
+
 
 # here's the homepage
 @app.route('/')
 def homepage():
-    return render_template("webapp_input.html")
-
-# example page for linking things
-@app.route('/example_linked')
-def linked_example():
-    return render_template("example_linked.html")
+	camera_catalog = pd.read_csv('./webapp/static/data/df_fix_summary.csv')
+	camera_list = (camera_catalog['brand'].apply(lambda x:x.capitalize())+' '+camera_catalog['model']).tolist()
+	return render_template("webapp_input.html",camera_list=camera_list)
 
 
 # now let's do something fancier - take an input, run it through a model, and display the output on a separate page
 
 @app.route('/webapp_input')
 def cam_price_input():
-   return render_template("webapp_input.html")
+	camera_catalog = pd.read_csv('./webapp/static/data/df_fix_summary.csv')
+	camera_list = (camera_catalog['brand'].apply(lambda x:x.capitalize())+' '+camera_catalog['model']).tolist()
+	return render_template("webapp_input.html",camera_list=camera_list)
 
 @app.route('/webapp_output')
 def cam_price_output():
-   # pull 'cam_model' from input field and store it
-   cam_model = request.args.get('cam_model')
+	# pull 'cam_model' from input field and store it
+	cam_model = request.args.get('cam_model')
+	brand = cam_model.split(' ', 1)[0]
+	model = cam_model.split(' ', 1)[1]
 
-   # read in our csv file
-   auc_db = pd.read_csv('./webapp/static/data/auc_price.csv')
-   fix_db = pd.read_csv('./webapp/static/data/fix_price.csv')
+	df_fix_summary = pd.read_csv('./webapp/static/data/df_fix_summary.csv')
+	model_summary = df_fix_summary[(df_fix_summary['brand']==brand.lower())&(df_fix_summary['model']==model)].iloc[0]
 
-   auc_prices = auc_db[auc_db['model'] == cam_model].values
-   fix_prices = fix_db[fix_db['model'] == cam_model].values
+	# random forest model
+	model_rf = pickle.load(open('./webapp/static/data/rf_fixedprice_binary_classification.pkl','rb'))
+	year = model_summary['year']
+	isDSLR = model_summary['isDSLR']
+	model_median = model_summary['model_median']
+	numAucListing = model_summary['numAucListing_median']
+	numFixListing = model_summary['numFixListing_median']
+	pricePercentile = 0.5
+	auc_median = model_summary['auc_median']
+	now = datetime.datetime.now()
+	startDayInWeek = now.isoweekday()
+	startHourInDay = now.hour
+	now_string = now.strftime("%Y-%m-%d %H:%M")
+	freeShipping = True
+	returnsAccepted = True
 
-   return render_template("webapp_output.html", auc_prices=auc_prices, fix_prices=fix_prices)
+
+	test_features = np.array([year,isDSLR,model_median,startDayInWeek,startHourInDay,
+	            numAucListing,numFixListing,pricePercentile,
+	            freeShipping,returnsAccepted,auc_median])
+
+	# make suggestion
+	if auc_median>=1.2 or not (np.argmax(model_rf.predict(test_features.reshape(1, -1)),axis=1)[0]):
+	    selling_option = 'auc'
+	    price = round(model_median*auc_median)
+	else:
+	    prices = 1.2+range(11)*(auc_median-1.2)/10
+	    for price in prices:
+	        test_features = np.array([year,isDSLR,model_median,startDayInWeek,startHourInDay,
+	                    numAucListing,numFixListing,pricePercentile,
+	                    freeShipping,returnsAccepted,price])
+	        if np.argmax(model_rf.predict(test_features.reshape(1, -1)),axis=1)[0]:
+	            selling_option = 'fix'
+	            price = round(model_median*price)
+	            break
+	return render_template("webapp_output.html",cam_model=cam_model, now_string =now_string, selling_option=selling_option, price=price)
